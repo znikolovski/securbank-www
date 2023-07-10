56cecf21959f370f6f39876a8f500e5fbdcc457a
Mark Szulc <mszulc@adobe.com>
2023-07-10 17:06:56 +1000

    content fragment ofer style

--- blocks/cfoffer/cfoffer.js
+++ blocks/cfoffer/cfoffer.js
@@ -2,10 +2,12 @@ export default async function decorate(block) {
    
     const aempublishurl = 'https://publish-p55117-e571178.adobeaemcloud.com'
     const persistedquery = '/graphql/execute.json/securbank/OfferByPath'
-    const cfpath = '/content/dam/securbank/en/offers/998'
-    const variationname = 'main'
+    const cfpath = '/content/dam/securbank/en/offers/'
+    
+    const offerid = block.querySelector(':scope div:nth-child(1) > div').innerHTML;
+    const variationname = block.querySelector(':scope div:nth-child(2) > div').innerHTML;
 
-    const url = aempublishurl + persistedquery + `;path=${cfpath};variation=${variationname};ts=${Math.random()*1000}`;
+    const url = aempublishurl + persistedquery + `;path=${cfpath}${offerid};variation=${variationname};ts=${Math.random()*1000}`;
     const options = {};
 
     const cfReq = await fetch(url, options)
@@ -20,8 +22,17 @@ export default async function decorate(block) {
         }
     });
 
-
-    block.innerHTML = "<h3>" + cfReq.headline + "</h3>";
+    block.innerHTML = `
+        <div class='banner-1170x200-content'>
+            <div class='banner-detail' style="background-image: linear-gradient(90deg,rgba(0,0,0,0.6), rgba(0,0,0,0.1) 80%) ,url(${cfReq.heroImage._publishUrl});">
+                <p class='pretitle'>${cfReq.pretitle}</p>
+                <p class='headline'>${cfReq.headline}</p>
+                <p class='detail'>${cfReq.detail.plaintext}</p>
+            </div>
+            <div class='banner-logo'>
+            </div>
+        </div>
+    `;
 
 
 }

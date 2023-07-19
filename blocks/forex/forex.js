4664664c8794fd3f2675b5cf2fef38475dced434
Mark Szulc <mszulc@adobe.com>
2023-07-19 13:29:17 +1000

    demo reset and lint fixes

--- blocks/forex/forex.js
+++ blocks/forex/forex.js
@@ -1,25 +1,18 @@
 export default async function decorate(block) {
-
     const container = document.createElement('ul');
-
-    const url = "https://20092-securbankdemo-stage.adobeio-static.net/api/v1/web/dx-excshell-1/forex";
+    const url = 'https://20092-securbankdemo-stage.adobeio-static.net/api/v1/web/dx-excshell-1/forex';
     const options = {};
-
     const forexReq = await fetch(url, options)
-
     const index = await forexReq.json();
-
     console.log(index);
     index.currencies
-        .forEach((currency) => {
-                const li = document.createElement('li');
-                li.innerHTML = `
-                <p>${currency.title}</p>
-                <p>${currency.forex}</p>
-                `;
-                container.append(li);
-        });
-
+    .forEach((currency) => {
+        const li = document.createElement('li');
+        li.innerHTML = `
+        <p>${currency.title}</p>
+        <p>${currency.forex}</p>
+        `;
+        container.append(li);
+    });
     block.append(container);
-    
 }
